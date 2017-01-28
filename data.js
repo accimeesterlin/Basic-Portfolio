11600a76c69d7ca1db0a8b314893600077d38322
imac <imac@imacs-iMac.local>
2017-01-28 00:03:29 -0500

    Finish work of basic portfolio

---
+++ data.js
@@ -0,0 +1,43 @@
+
+// Adding text after the hover effects from the DOM
+var para = function(e){
+
+	var data = document.getElementById('data');
+
+	data.innerText = 'Pease visit me on ' + e.alt + '...';
+	// data.style.display = 'block';
+};
+
+
+// Deleting the innerText inside  below the icons
+data.addEventListener('mouseout', function(){
+	data.innerText = '';
+});
+
+
+
+// Changing the connect color style every 2 seconds
+var colors = ['red', 'blue', 'orange', 'green', 'purple', 'lightgrey', 'black'],
+	connect = document.getElementById('connect');
+
+setInterval(function(){
+		connect.style.color = colors[Math.round(Math.random() * colors.length)];
+}, 2000);
+
+
+
+// Gradding the user input from the Contact page
+var person = document.getElementById('name'),
+	email = document.getElementById('email'),
+	message = document.getElementById('message'),
+	button = document.getElementById('button'),
+	content = document.getElementById('content');
+
+// Sending message
+	button.addEventListener('click', function(){
+		alert('Your message has been sent!');
+		person.value = '';
+		email.value = '';
+		message.value = '';
+
+	});
